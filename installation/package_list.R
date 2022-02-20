c06113a81ad56dc54b257b68b2b5961b718860b3
lapotok <lapotok@gmail.com>
2022-02-20 16:33:36 +0300

    Package group annotations

--- installation/package_list.R
+++ installation/package_list.R
@@ -1,5 +1,43 @@
 # пакеты для установки командой install.packages(base.packages)
-base.packages <<- c("Amelia","boot","broom","car","caret","corrplot","cowplot","curl","devtools","remotes","esquisse","furrr","gdtools","GGally","ggforce","ggplot2","ggpubr","ggrepel","ggridges","ggstance","ggthemes","glue","gridExtra","gtools","hexbin","httr","investr","jsonlite","kableExtra","knitr","lme4","lmtest","lubridate","magrittr","mice","missForest","modelr","mvtnorm","naniar","officer","openxlsx","plotly","prettydoc","profvis","purrr","randomForest","readr","readxl","repr","reprex","reshape","reshape2","rio","rmarkdown","rvest","rvg","sandwich","scales","selectr","shiny","sinaplot","sjPlot","tidyverse","multcomp","drc","RColorBrewer","nlme","caret","ipred","effsize","dunn.test","magrittr","remedy","ggalt","ggExtra","gsheet","jtools", "ggmosaic", "vcd", 'htmltools', 'jquerylib', 'rmarkdown', 'xfun')
+base.packages <<- c(
+  # general stuff
+  "tidyverse", "magrittr", "glue", "scales", "purrr", "furrr", "profvis", 
+  
+  # package installation and management
+  "curl","devtools","remotes", 
+  
+  # data handling
+  "lubridate", "reshape","reshape2", "scales", 
+  
+  # file import
+  "openxlsx", "gsheet", "readr","readxl", "rio", 
+  
+  # models
+  "investr", "lme4","lmtest","mice","missForest","modelr",
+  "mvtnorm","naniar", "multcomp","drc", "nlme","caret", 
+  "sandwich", "broom","car","caret", "randomForest",
+  
+  # web 
+  "httr","jsonlite", "rvest", 'htmltools', 'jquerylib', 
+  
+  # reports
+  "rmarkdown", "knitr","prettydoc","repr","reprex", "shiny", 
+  
+  # plots
+  "ggpubr","ggrepel","ggridges","ggstance","ggthemes",
+  "corrplot","cowplot","plotly", "rvg", "ggalt","ggExtra", 
+  "officer", "ggmosaic", "esquisse", "GGally","ggforce", 
+  
+  # misc
+  "Amelia","boot","gdtools","gridExtra","gtools","hexbin","kableExtra",
+  "selectr", "sinaplot","sjPlot","RColorBrewer","ipred","effsize","dunn.test",
+  "remedy", "jtools",  "vcd",  'xfun' 
+)
 
 # пакеты для установки командой remotes::install_github(base.packages)
-github.packages <<- c("patchwork"="thomasp85/patchwork", "crayon"="r-lib/crayon", "rstatix"="kassambara/rstatix", "export"="tomwenseleers/export")
+github.packages <<- c(
+  "patchwork"="thomasp85/patchwork", # combine images
+  "crayon"="r-lib/crayon",           # colored text in terminal
+  "rstatix"="kassambara/rstatix",    # tidy stats
+  "export"="tomwenseleers/export"    # plots -> pptx
+  )
